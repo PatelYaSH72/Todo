2e296f0e85af5d372743012a27f1755705f8a065
PatelYaSH72 <yp7201007221@gmail.com>
2026-04-14 15:46:56 +0530

    updated project code

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000/api'
+const API_BASE_URL = 'https://todo-wg2w.vercel.app/api'
 
 // Create axios instance
 const api = axios.create({
